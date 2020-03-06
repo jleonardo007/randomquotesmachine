39517ad6dcf2e5e1d4f3c3631983dd21dd240d91
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-03-06 12:12:05 -0400

    Updates

---
+++ precache-manifest.677b7cf9c4afc7937552e609011c6087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605bc5dbefd454b2e841d0c46066fe08",
+    "url": "/jleonardo007/randomquotesmachine/index.html"
+  },
+  {
+    "revision": "e9a5e099a85727b8faa3",
+    "url": "/jleonardo007/randomquotesmachine/static/css/main.c0ffd346.chunk.css"
+  },
+  {
+    "revision": "60f3b8e8fb0ec9c72cb7",
+    "url": "/jleonardo007/randomquotesmachine/static/js/2.032cf61d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/jleonardo007/randomquotesmachine/static/js/2.032cf61d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a5e099a85727b8faa3",
+    "url": "/jleonardo007/randomquotesmachine/static/js/main.a54b9df0.chunk.js"
+  },
+  {
+    "revision": "b6567fda400fa80bee2a",
+    "url": "/jleonardo007/randomquotesmachine/static/js/runtime-main.95933fa5.js"
+  }
+]);
